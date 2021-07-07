bc90ff5182127ca6ac2b1faeece73a36b440e5e8
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2021-07-07 14:31:53 +0200

    Initial prototype

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,19 @@
 //     Dr. Maxim Orlovsky <orlovsky@pandoracore.com>
 // for Pandora Core AG
 
-mod lexer;
-mod parser;
+#[macro_use]
+extern crate pest_derive;
 
+use std::fs::File;
+use std::io::Read;
 use std::path::PathBuf;
 
 use clap::{AppSettings, Clap};
+use pest::Parser;
+
+#[derive(Parser)]
+#[grammar = "../grammar/aluasm.pest"]
+pub struct AsmParser;
 
 #[derive(Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Debug, Clap)]
 #[clap(
@@ -25,4 +32,21 @@ pub struct Args {
     pub files: Vec<PathBuf>,
 }
 
-fn main() { let args = Args::parse(); }
+fn main() {
+    let args = Args::parse();
+
+    for file in args.files {
+        let mut fd = File::open(file).expect("wrong file");
+
+        let mut s = String::new();
+        fd.read_to_string(&mut s).expect("reading file");
+        let pairs =
+            AsmParser::parse(Rule::program, &s).expect("compilation error: ");
+
+        for pair in pairs {
+            println!("Rule:    {:?}", pair.as_rule());
+            println!("Span:    {:?}", pair.as_span());
+            println!("Text:    {}", pair.as_str());
+        }
+    }
+}
