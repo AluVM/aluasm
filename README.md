2bd55ecfca8afe95fb8d0a03d408972f27df788e
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2021-07-31 02:35:50 +0200

    Release v0.1.0

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ may be used for producing AluVM and AluRE libraries and executable files.
 
 For examples, check `./examples` folder and see how they are compiled by using
 `./test/code_build.sh` script.
+
+You can read more about AluVM on <https://www.aluvm.org>
--- test/code_build.sh
+++ test/code_build.sh
@@ -10,11 +10,10 @@ $ASM $ASM_FLAGS examples/all.aluasm
 $ASM $ASM_FLAGS examples/miner.aluasm
 $ASM $ASM_FLAGS examples/pow.aluasm
 $ASM $ASM_FLAGS examples/pedersen.aluasm
-$ASM $ASM_FLAGS examples/rgb20.aluasm
+# $ASM $ASM_FLAGS examples/rgb20.aluasm
 
 $LINK $LINK_FLAGS --org=lnpbp.org --bin all
 $LINK $LINK_FLAGS --org=pandoracore.org --lib miner
 $LINK $LINK_FLAGS --org=pandoracore.org --bin pow
 $LINK $LINK_FLAGS --org=lnpbp.org --lib pedersen
-$LINK $LINK_FLAGS --org=lnpbp.org --bin rgb20
-
+# $LINK $LINK_FLAGS --org=lnpbp.org --bin rgb20
